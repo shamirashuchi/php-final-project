e68f39d255165e5ab7cc6dafef1c2f68ca8cc3d1
shamirashuchi <shamirashuchi@gmail.com>
2023-11-16 20:43:00 +0600

    css-struchture

--- views/header.php
+++ views/header.php
@@ -6,84 +6,74 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Home page</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <style>
+        /*h1*/
+        /* {*/
+        /*     background-color: red;*/
+        /*     color: white;*/
+        /*     font-size:70px;*/
+        /* }*/
+        /*.test*/
+        /*{*/
+        /*    background-color: red;*/
+        /*    color: white;*/
+        /*    font-size:70px;*/
+        /*}*/
+        /***/
+        /*{*/
+        /*    background-color: red;*/
+        /*    color: white;*/
+        /*    font-size:70px;*/
+        /*}*/
+    </style>
+
 </head>
 <body>
 <!--compiler of html is browser-->
+<!--css is class based stylesheet language.
+css class basic syntax:
+selector
+{
+property:value;
+property:value;
+property:value;
+}
+
+selector can be:
+html tag,user define,special syntax
+
+
+css class example:
+
+.html,.css,.php
+html tags block label and inline.
+h1 and p is block label.
+a and span inline. jototuku content tototuku.float:left make inline
+height:300px,border:300px,padding:300px; equal to height:300px dile egula value pay
+box-sizing:border-box; height er padding border bad dey
+---------->
 
 
 
 
 
 
-<ul>
-    <li><a href="web.php?page=home">Home</a></li>
-    <li><a href="web.php?page=about">About</a></li>
-    <li><a href="web.php?page=contact">Contact</a></li>
-</ul>
-
-
-
-
-
-
-
-
-
-
-
-
-<!--<input type="text">-->
-<!--<textarea></textarea>-->
-<!--<select>-->
-<!--    <option>Select District -----</option>-->
-<!--    <option>Dhaka</option>-->
-<!--    <option>Manikgong</option>-->
-<!--    <option>Dhaka</option>-->
-<!--    <option>Manikgong</option>-->
-<!--</select>-->
-<!--<input type="radio"/>-->
-<!--<input type="checkbox"/>-->
-<!--<input type="date"/>-->
-<!--<input type="file"/>-->
-<!---->
-<!--<ul>-->
-<!--    <li>-->
-<!--        Dhaka-->
-<!--        <ul>-->
-<!--            <li>Dhanmondi</li>-->
-<!--            <li>-->
-<!--                Mirpur-->
-<!--                <ul>-->
-<!--                    <li>Kajipura</li>-->
-<!--                    <li>Shaaawrapara</li>-->
-<!--                    <li>Rupnogr</li>-->
-<!---->
-<!--                </ul>-->
-<!--            </li>-->
-<!--            <li>Khilgaon</li>-->
-<!--            <li>Savar</li>-->
-<!--        </ul>-->
-<!--    </li>-->
-<!--    <li>Manikgong</li>-->
-<!--    <li>Gazipur</li>-->
-<!--    <li>Narayongong</li>-->
-<!--    <li>Noakhali</li>-->
+<!--<ul class="menu">-->
+<!--    <li><a href="web.php?page=home">Home</a></li>-->
+<!--    <li><a href="web.php?page=about">About</a></li>-->
+<!--    <li><a href="web.php?page=contact">Contact</a></li>-->
 <!--</ul>-->
 <!---->
-<!---->
-<!---->
-<!---->
-<!--<ol>-->
-<!--    <li>Mango</li>-->
-<!--    <li>-->
-<!--        Jackfruit-->
-<!--        <ol>-->
-<!--            <li>Mango</li>-->
-<!--            <li>Jackfruit</li>-->
-<!--            <li>Mango</li>-->
-<!--            <li>Jackfruit</li>-->
-<!--        </ol>-->
-<!--    </li>-->
-<!--    <li>Mango</li>-->
-<!--    <li>Jackfruit</li>-->
-<!--</ol>-->
+<!--<br/>-->
+<!--<br/>-->
+
+
+
+
+
+
+
+
+
+
--- views/home.php
+++ views/home.php
@@ -1,25 +1,127 @@
 <?php include "header.php"; ?>
 
-<!--target="_blank" link new tab e open hoy-->
-<a href="https://prothomalo.com/" target="_blank">BITM</a>
-<h1>This is heading</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor dolorum ratione totam unde, vel voluptatibus. Accusantium distinctio ducimus eligendi error itaque, laboriosam maiores nam, quae, repellendus suscipit vero voluptatum.</p>
-<img src="assets/img/1.jpeg" alt="" height="300" width="400"/>
 
-<table border="1 width" = '888'>
-    <tr>
-        <th>Sl NO</th>
-        <th>Name</th>
-        <th>Email</th>
-        <th>Mobile</th>
-    </tr>
-  <?php foreach ($students as $key => $student) { ?>
-    <tr>
-        <td><?php echo ++$key; ?></td>
-        <td><?php echo $student['name']; ?></td>
-        <td><?php echo $student['email']; ?></td>
-        <td><?php echo $student['mobile']; ?></td>
-    </tr>
-<?php } ?>
-</table>
+
+<div class="div-one">
+        Hello world
+</div>
+
+<div class="div-two">
+
+</div>
+
+<div class="div-three">
+
+</div>
+
+<div class="div-four">
+
+</div>
+
+<div class="div-five">
+
+</div>
+
+<div class="div-six">
+
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
 <?php include "footer.php"; ?>
+
+
+<!--target="_blank" link new tab e open hoy-->
+<!--<a href="https://prothomalo.com/" target="_blank">BITM</a>-->
+<!--#test for id-->
+<!--    <h1> id="test">This is heading</h1>-->
+<!--    <!--.test for id-->
+<!--* is for every design-->
+
+
+<!---->
+<!---->
+<!--    <input type="text">-->
+<!--    <textarea></textarea>-->
+<!--    <select>-->
+<!--        <option>Select District -----</option>-->
+<!--        <option>Dhaka</option>-->
+<!--        <option>Manikgong</option>-->
+<!--        <option>Dhaka</option>-->
+<!--        <option>Manikgong</option>-->
+<!--    </select>-->
+<!--    <input type="radio"/>-->
+<!--    <input type="checkbox"/>-->
+<!--    <input type="date"/>-->
+<!--    <input type="file"/>-->
+<!---->
+<!--    <ul>-->
+<!--        <li>-->
+<!--            Dhaka-->
+<!--            <ul>-->
+<!--                <li>Dhanmondi</li>-->
+<!--                <li>-->
+<!--                    Mirpur-->
+<!--                    <ul>-->
+<!--                        <li>Kajipura</li>-->
+<!--                        <li>Shaaawrapara</li>-->
+<!--                        <li>Rupnogr</li>-->
+<!---->
+<!--                    </ul>-->
+<!--                </li>-->
+<!--                <li>Khilgaon</li>-->
+<!--                <li>Savar</li>-->
+<!--            </ul>-->
+<!--        </li>-->
+<!--        <li>Manikgong</li>-->
+<!--        <li>Gazipur</li>-->
+<!--        <li>Narayongong</li>-->
+<!--        <li>Noakhali</li>-->
+<!--    </ul>-->
+<!---->
+<!---->
+<!---->
+<!---->
+<!--    <ol>-->
+<!--        <li>Mango</li>-->
+<!--        <li>-->
+<!--            Jackfruit-->
+<!--            <ol>-->
+<!--                <li>Mango</li>-->
+<!--                <li>Jackfruit</li>-->
+<!--                <li>Mango</li>-->
+<!--                <li>Jackfruit</li>-->
+<!--            </ol>-->
+<!--        </li>-->
+<!--        <li>Mango</li>-->
+<!--        <li>Jackfruit</li>-->
+<!--    </ol>-->
+<!--<h1 class="test">This is heading</h1>-->
+<!--<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Corporis dolor dolorum ratione totam unde, vel voluptatibus. Accusantium distinctio ducimus eligendi error itaque, laboriosam maiores nam, quae, repellendus suscipit vero voluptatum.</p>-->
+<!--<img src="assets/img/1.jpeg" alt="" height="300" width="400"/>-->
+<!---->
+<!--<table border="1 width" = '888'>-->
+<!--    <tr>-->
+<!--        <th>Sl NO</th>-->
+<!--        <th>Name</th>-->
+<!--        <th>Email</th>-->
+<!--        <th>Mobile</th>-->
+<!--    </tr>-->
+<!--  --><?php //foreach ($students as $key => $student) { ?>
+<!--    <tr>-->
+<!--        <td>--><?php //echo ++$key; ?><!--</td>-->
+<!--        <td>--><?php //echo $student['name']; ?><!--</td>-->
+<!--        <td>--><?php //echo $student['email']; ?><!--</td>-->
+<!--        <td>--><?php //echo $student['mobile']; ?><!--</td>-->
+<!--    </tr>-->
+<?php //} ?>
+<!--</table>-->
